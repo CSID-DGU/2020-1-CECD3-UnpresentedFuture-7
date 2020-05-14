522e290201a7df1c492f0374504dac156bd96ad3
odyflame <ieehyeon@naver.com>
2020-05-14 21:54:58 +0900

    update sungoolim's project.
    update target's speed. add collider.
    add yunu's library and trail

---
+++ prac/Assets/script/Speed.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speed : Movement
+{
+    // Start is called before the first frame update
+    public bool init_as_local_speed = false;
+
+
+    public override Vector3 CurrentState()
+    {
+        if (ApplyAsLocalSpeed)
+            return TargetTransform.localPosition;
+        else
+            return TargetTransform.position;
+    } 
+    public override void SetState(Vector3 state, MonoBehaviour caller)
+    {
+        if (ApplyAsLocalSpeed)
+            TargetTransform.localPosition = state;
+        else
+            TargetTransform.position= state;
+    }
+
+    void Awake()
+    {
+        if (init_as_local_speed)
+        {
+            speed = TargetTransform.right * speed.x + TargetTransform.up * speed.y + TargetTransform.forward * speed.z;
+            acceleration = TargetTransform.right * acceleration.x + TargetTransform.up * acceleration.y + TargetTransform.forward * acceleration.z;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ApplyAsLocalSpeed)
+            TargetTransform.localPosition += speed * Time.deltaTime;
+        else
+            TargetTransform.position += speed * Time.deltaTime;
+        speed += acceleration * Time.deltaTime;
+    }
+}
---
+++ prac/Assets/script/SpeedRotSync.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedRotSync : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform follower;
+    public Speed target;
+    void Start()
+    {
+        
+    }
+    void Reset()
+    {
+        target = gameObject.GetComponent<Speed>();
+        follower=gameObject.GetComponent<Transform>();
+        follower.rotation = Quaternion.LookRotation(target.speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        follower.rotation = Quaternion.LookRotation(target.speed);
+    }
+}
