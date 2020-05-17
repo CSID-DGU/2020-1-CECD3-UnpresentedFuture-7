0cb2ee13093559fe68a05ebb0d1a9ea0114676f4
kkiyshkk0 <kkiyshkk0@gmail.com>
2020-05-18 00:00:46 +0900

    #20-05-18 #시작 씬 기본 틀 구현

--- prac/Assets/script/StartSceneScript/StartSceneManager.cs
+++ prac/Assets/script/StartSceneScript/StartSceneManager.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class StartSceneManager : MonoBehaviour
-{
+public class StartSceneManager: MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
+    }
+
+    //게임 화면으로 씬 이동
+    public void Game_Start() {
+        SceneManager.LoadScene("punch");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag=="hand") { 
+            SceneManager.LoadScene("punch");
+        }
+       
     }
 }
