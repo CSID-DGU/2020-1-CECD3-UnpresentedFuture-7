cd9f3f2b96ef2b40193cd33b2858fae7e67445fe
staris <sungdoolim@naver.com>
2020-05-17 20:56:36 +0900

    mesh event

--- prac/Assets/script/target.cs
+++ prac/Assets/script/target.cs
@@ -32,19 +32,33 @@ public class target : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag=="hand")
-        { this.gameObject.SetActive(false);
+        {// this.gameObject.SetActive(false);
 
             controller.score++;
+            //  GetComponent<Speed>().speed = collision.gameObject.GetComponent<FistSpeed>().CurrentSpeed;
 
-            Debug.Log("a: " + controller.score);
-            Invoke("f",2f);
+            // speed라는게 원래 있는 건가 보데..?
+     
+            /*   Vector3 CurrentSpeed=GetComponent<FistSpeed>().CurrentSpeed;// 이런식으로 가져오는 거구나...
+            if (CurrentSpeed.magnitude > 10)
+            {
+               
+            }
+            */
+            GetComponent<MeshExploder>().Explode();// 깨지는 이미지 구현한거 
+            //  Instantiate<GameObject>(AfterEffect,transform,true); // 폭발 하는거 
+
+            Debug.Log("score: " + controller.score);
+            Invoke("f", 2f);
             Debug.Log("colli : " + collision.gameObject.tag);
         }
        
     }
     private void f()
     {
+        this.gameObject.SetActive(false);
         this.transform.position = original_p;
         this.gameObject.SetActive(true);
+        transform.position = Vector3.MoveTowards(transform.position, target_b, 0.1f);
     }
 }
