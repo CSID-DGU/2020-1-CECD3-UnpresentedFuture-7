8649a4b673434d023ace6ceafbd0cb8b1267d458
staris <sungdoolim@naver.com>
2020-05-25 20:10:27 +0900

    initiate obj

--- prac/Assets/script/controller.cs
+++ prac/Assets/script/controller.cs
@@ -6,15 +6,32 @@ public class controller : MonoBehaviour
 {
 
     public static int score=0;
+
+    public GameObject firstprefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        Invoke("continueing", 2);
+    }
+    void Update()
+    {
+    }
+
+    private void rout()
+    {
+        while (true)
+        {
+            
+        }
+    }
+    private void continueing()
+    {
+        Instantiate(firstprefab, new Vector3(-20,-3,-5), Quaternion.identity);
+
+        Invoke("continueing", 2);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 }
--- prac/Assets/script/target.cs
+++ prac/Assets/script/target.cs
@@ -7,6 +7,7 @@ public class target : MonoBehaviour
     Rigidbody r;
     public Transform box;
     public Transform box2;
+  //  public GameObject firstprefab;
    // public controller ct;
     Vector3 target_b;
     Vector3 original_p;
@@ -14,8 +15,8 @@ public class target : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      
-        
+
+        //firstprefab = GetComponent<GameObject>();
         r = GetComponent<Rigidbody>();
         target_b = (box.position + box2.position) / 2;
         original_p = transform.position;
@@ -28,7 +29,9 @@ public class target : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position,target_b , 0.1f);
         if (transform.position == target_b)
         {
-            Invoke("f", 2f);
+            GetComponent<MeshExploder>().Explode(); this.gameObject.SetActive(false);
+            Invoke("f", 2f); 
+
         }
 
     }
@@ -38,46 +41,45 @@ public class target : MonoBehaviour
         //GetComponent<MeshRenderer>().enabled = false;
         if (collision.gameObject.tag=="hand")
         {
-            
-            
             controller.score++;
             //  GetComponent<Speed>().speed = collision.gameObject.GetComponent<FistSpeed>().CurrentSpeed;
-
             // speed라는게 원래 있는 건가 보데..?
-     
             /*   Vector3 CurrentSpeed=GetComponent<FistSpeed>().CurrentSpeed;// 이런식으로 가져오는 거구나...
             if (CurrentSpeed.magnitude > 10)
             {
-               
             }
             */
             GetComponent<MeshExploder>().Explode();// 깨지는 이미지 구현한거 
             //  Instantiate<GameObject>(AfterEffect,transform,true); // 폭발 하는거 
             this.gameObject.SetActive(false);
-
             Debug.Log("score: " + controller.score);
             Invoke("f", 2f);
             Debug.Log("colli : " + collision.gameObject.tag);
         }
        
     }
+
     private void f()
     {
-        this.gameObject.SetActive(false);
+        
         this.transform.position = original_p;
         this.gameObject.SetActive(true);
         transform.position = Vector3.MoveTowards(transform.position, target_b, 0.1f);
     }
+    private void OnDisable()
+    {
+        //original_p = original_p * Random.Range(0.5f, 1f);
+        original_p = new Vector3(original_p.x * Random.Range(0.8f, 1.1f), original_p.y * Random.Range(0f, 2f), original_p.z * Random.Range(0f, 2f));
+        r.velocity = new Vector3(0, 0, 0);
+        r.ResetInertiaTensor();
+        
+    }
     private void OnEnable()
     {
+        
         target_b = (box.position + box2.position) / 2;
         transform.position = Vector3.MoveTowards(transform.position, target_b, 0.1f);
 
     }
-    private void OnDisable()
-    {
-        //original_p = original_p * Random.Range(0.5f, 1f);
-        original_p =new Vector3(original_p.x * Random.Range(0.8f, 1.1f), original_p.y * Random.Range(0f, 2f), original_p.z * Random.Range(0f, 2f));
-        r.velocity = new Vector3(0, 0, 0);
-    }
+
 }
