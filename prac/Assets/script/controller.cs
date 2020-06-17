367030eb0b7b3d0d223917cb40627c82c6f7d8b8
Yoon_Gi_Chul <kkiyshkk0@naver.com>
2020-06-17 20:11:20 +0900

    #2020-06-17 #게임 중 점수 출력

--- prac/Assets/script/controller.cs
+++ prac/Assets/script/controller.cs
@@ -5,34 +5,35 @@ using UnityEngine.UI;
 
 public class controller : MonoBehaviour
 {
-    
-    public Text score_text;
-    public static int score=0;
-    Vector3 original_p;
 
-    public GameObject firstprefab;
+    public Text score_text;         // 점수 텍스트 객체
+    public static int score = 0;    // 게임 점수
+    public GameObject firstprefab;  // 타격 오브젝트
+    private Vector3 original_p;     // 타격 오브젝트 위치
+    public float delaytime;         // 타격 오브젝트 생성 속도
+
     // Start is called before the first frame update
     void Start()
     {
-        original_p = new Vector3(-15f,-3f,-5f);
-        Invoke("continueing", 1);
+        original_p = new Vector3(0.0f, 5.0f, 30.0f);     // 처음 타격 오브젝트의 위치
+        StartCoroutine(continueing());              // 타격 오브젝트를 delaytime마다 생성
+        delaytime = 2.0f;
     }
     void Update()
     {
+        score_text.text = "score : " + score;       // 점수 갱신
     }
 
-    private void rout()
+    IEnumerator continueing()
     {
-
+        while (true)
+        {
+            // 랜덤한 위치에 따라 타격 오브젝트 생성
+            Instantiate(firstprefab, new Vector3(original_p.x + Random.Range(-30f, 30f),
+                                                original_p.y + Random.Range(-5f, 5f),
+                                                original_p.z + Random.Range(-10f, 10f)),
+                                                Quaternion.identity);
+            yield return new WaitForSeconds(delaytime);
+        }
     }
-    private void continueing()
-    {
-        Instantiate(firstprefab, new Vector3(original_p.x*Random.Range(-3f,3f),original_p.y*Random.Range(-5f,5f),original_p.z*Random.Range(-5f,5f)), Quaternion.identity);
-
-        Invoke("continueing", 1);
-    }
-
-    // Update is called once per frame
- 
-
 }
