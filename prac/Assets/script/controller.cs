3076b8acd87945aa8715eff02da619eec3933c07
staris <sungdoolim@naver.com>
2020-06-19 19:24:35 +0900

    betagit add .git add .

---
+++ prac/Assets/script/controller.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class controller : MonoBehaviour
+{
+
+    public Text score_text;
+    public Text level_text;
+    public Text heart_text;
+    public static int level = 1;
+    public static int score = 0;
+    public static float time = 0;// 0618 성두 static
+    public GameObject firstprefab;
+    Vector3 original_p;
+    public Canvas menu;
+    private bool isopen = false;
+
+    public float delayTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 60f;
+        original_p = new Vector3(0.0f, 5.0f, 10f);
+        StartCoroutine(continueing());
+        delayTime = 2.0f;
+
+    }
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time < 0)
+        {
+            Time.timeScale = 0;
+        }
+        //  Debug.Log("heart : " + time);
+        if (Input.GetKeyDown("escape"))
+        {
+            isopen = !isopen;
+            if (isopen)
+            {
+                Time.timeScale = 0.001f;
+                menu.enabled = true;
+                //Instantiate(menu);
+            }
+            else
+            {
+                menu.enabled = false;
+                Time.timeScale = 1f;
+            }
+        }
+
+        score_text.text = "score : " + score;   // 점수 갱신
+        level_text.text = "level : " + level;   // 점수 갱신
+        heart_text.text = "heart : " + Mathf.Ceil(time).ToString(); //라이프 갱신
+    }
+    
+    IEnumerator continueing()
+    {
+        while (true)
+        {
+            // 랜덤한 위치에 따라 타격 오브젝트 생성
+            Instantiate(firstprefab, new Vector3(original_p.x + Random.Range(-30f, 30f),
+                                                original_p.y + Random.Range(-5f, 5f),
+                                                original_p.z + Random.Range(-10f, 10f)),
+                                                Quaternion.identity);
+            yield return new WaitForSeconds(delayTime);
+        }
+    }
+}
---
+++ prac/Assets/script/target.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class target : MonoBehaviour
+{
+    Rigidbody r;
+    Vector3 target_b;
+    Vector3 original_p;
+    GameObject targettmp;//---------------------------------------0618 성두
+    int hp;//-----------------------------------------------------0618 성두
+    int maxhp;//--------------------------------------------------0618 성두
+    int target_speed;
+    Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = gameObject.GetComponent<Renderer>();
+        hp = (int)Random.Range(1f, 7f);//-------------------------0618 성두
+        maxhp = hp;
+        switch (hp)
+        {
+            case 1:
+                renderer.material.color = Color.white;
+                break;
+            case 2:
+                renderer.material.color = Color.red;
+                break;
+            case 3:
+                renderer.material.color = Color.cyan;
+                break;
+            case 4:
+                renderer.material.color = Color.green;
+                break;
+            case 5:
+                renderer.material.color = Color.blue;
+                break;
+         
+            default:
+                renderer.material.color = Color.black;
+                break;
+        }
+        r = GetComponent<Rigidbody>();
+        target_b = new Vector3(0.0f, 5.0f, 0.1f);
+
+        original_p = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //  targettmp = GameObject.FindGameObjectWithTag("target");
+    //    target_b = targettmp.transform.position;
+
+        // 타격 오브젝트 이동
+        transform.position = Vector3.MoveTowards(transform.position, target_b, 10.0f * Time.deltaTime);
+        // 지정된 위치에 타격 오브젝트 도착시 객체 소멸
+        if (transform.position == target_b)
+        {
+            this.gameObject.SetActive(false);
+            Destroy(this.gameObject);
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Hand 객체와 충돌 시 타격 오브젝트 파괴
+        if (collision.gameObject.tag == "hand")
+        {
+            hp -= 1;//-----------------------------------------------------------------0618 성두
+            switch (hp)
+            {
+                case 1:
+                    renderer.material.color = Color.white;
+                    break;
+
+                case 2:
+                    renderer.material.color = Color.red;
+                    break;
+                case 3:
+                    renderer.material.color = Color.cyan;
+                    break;
+                case 4:
+                    renderer.material.color = Color.green;
+                    break;
+                case 5:
+                    renderer.material.color = Color.blue;
+                    break;
+
+                default:
+                    renderer.material.color = Color.black;
+                    break;
+            }
+
+            if (hp <= 0)//-----------------------------------------------------------------0618 성두
+            {
+                controller.time += maxhp;
+                controller.score += maxhp;
+
+              
+                    controller.level = (controller.score / 5) +1;
+                
+                    GetComponent<MeshExploder>().Explode();// 깨지는 이미지 구현한거 
+                this.gameObject.SetActive(false);
+                Destroy(this.gameObject);
+            }
+
+            Debug.Log("collision : " + collision.gameObject.tag);
+        }
+    }
+}
