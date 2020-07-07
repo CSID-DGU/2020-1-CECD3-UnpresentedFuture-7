4abd430ad932e8c09a3f6226a42cceb879535f0d
staris <sungdoolim@naver.com>
2020-07-07 17:37:56 +0900

    merge with final_master 0707

---
+++ TestLeap/Assets/script/controller.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class controller : MonoBehaviour
+{
+
+    public Text score_text;
+    public Text level_text;
+    public static int level = 1;
+    public static int score = 0;
+    public static float heart = 0;// 0618 성두 static
+    public GameObject firstprefab;
+    public GameObject secondprefab;
+    public GameObject thirdprefab;
+    Vector3 original_p;
+    public Canvas menu;
+   private bool isopen = false;
+     public Canvas menu_gameover;
+     private bool isover=false;
+
+    public float delayTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        heart = 30f;
+        original_p = new Vector3(0.0f, 5.0f, 10f);
+        StartCoroutine(continueing());
+        delayTime = 3.0f;
+        
+                menu_gameover.enabled = false;
+
+    }
+    void Update()
+    {
+        heart -= Time.deltaTime;
+        if (heart < 0)
+        {
+            Time.timeScale = 0;
+
+                menu_gameover.enabled = true;
+                //Instantiate(menu);
+
+        }
+        if (Input.GetKeyDown("escape"))
+        {
+            isopen = !isopen;
+            if (isopen)
+            {
+                Time.timeScale = 0.001f;
+                menu.enabled = true;
+                //Instantiate(menu);
+            }
+            else
+            {
+                menu.enabled = false;
+                Time.timeScale = 1f;
+            }
+        }
+        
+
+        score_text.text = "score : " + score;   // 점수 갱신
+        level_text.text = "Lv : " + level;   // 점수 갱신
+    }
+    
+
+  
+
+    IEnumerator continueing()
+    {
+        while (true)
+        {float delay=delayTime/level;
+            // 랜덤한 위치에 따라 타격 오브젝트 생성
+            if(delay<1.5f)
+            delay=1.5f;
+            
+            Instantiate(firstprefab, new Vector3(original_p.x + Random.Range(-30f, 30f),
+                                                original_p.y + Random.Range(-5f, 5f),
+                                                original_p.z + Random.Range(-10f, 10f)),
+                                                Quaternion.identity);
+            if(level>4){
+                Instantiate(secondprefab, new Vector3(original_p.x + Random.Range(-30f, 30f),
+                                                original_p.y + Random.Range(-5f, 5f),
+                                                original_p.z + Random.Range(-10f, 10f)),
+                                                Quaternion.identity);
+                                                delay=2.0f;
+                                               
+                                                
+            } if(level>9){
+                Instantiate(thirdprefab, new Vector3(original_p.x + Random.Range(-30f, 30f),
+                                                original_p.y + Random.Range(-5f, 5f),
+                                                original_p.z + Random.Range(-10f, 10f)),
+                                                Quaternion.identity);
+                                                delay=2.5f;
+                                               
+                                                
+            }
+
+            
+            
+
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
---
+++ TestLeap/Assets/script/target.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class target : MonoBehaviour
+{
+    Rigidbody r;
+    Vector3 target_b;
+    Vector3 original_p;
+    GameObject targettmp;//---------------------------------------0618 성두
+    int hp;
+    int maxhp;
+    float target_speed;
+    Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        target_speed=10f*(1+((float)controller.level/20f));
+
+        renderer = gameObject.GetComponent<Renderer>();
+        hp = (int)Random.Range(1f, 4f);
+        maxhp = hp;
+        switch (hp)
+        {
+            case 1:
+                renderer.material.color = Color.white;
+                break;
+            case 2:
+                renderer.material.color = Color.red;
+                break;
+  
+         
+            default:
+                renderer.material.color = Color.black;
+                break;
+        }
+        r = GetComponent<Rigidbody>();
+        target_b = new Vector3(0.0f, 5.0f, 0.1f);
+
+        original_p = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //  targettmp = GameObject.FindGameObjectWithTag("target");
+    //    target_b = targettmp.transform.position;
+
+        // 타격 오브젝트 이동
+        transform.position = Vector3.MoveTowards(transform.position, target_b, target_speed * Time.deltaTime);
+        // 지정된 위치에 타격 오브젝트 도착시 객체 소멸
+        if (transform.position == target_b)
+        {
+            this.gameObject.SetActive(false);
+            controller.heart -= this.hp;
+            Destroy(this.gameObject);
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Hand 객체와 충돌 시 타격 오브젝트 파괴
+        if (collision.gameObject.tag == "hand")
+        {
+            hp -= 1;
+            switch (hp)
+            {
+                case 1:
+                    renderer.material.color = Color.white;
+                    break;
+
+                case 2:
+                    renderer.material.color = Color.red;
+                    break;
+                default:
+                    renderer.material.color = Color.black;
+                    break;
+            }
+
+            if (hp <= 0)
+            {
+                controller.heart += maxhp;
+                controller.score += maxhp;
+
+              
+                    controller.level = (controller.score / 5) +1;
+                    
+                
+                    GetComponent<MeshExploder>().Explode();// 깨지는 이미지 구현한거 
+                this.gameObject.SetActive(false);
+                Destroy(this.gameObject);
+            }
+
+            Debug.Log("collision : " + collision.gameObject.tag);
+        }
+    }
+}
