baa06b71f1e46ac5e6b599bd9b67f1a7e5d32ff1
staris <sungdoolim@naver.com>
2020-07-20 22:37:35 +0900

    track hands

---
+++ TestLeap/Assets/script/gloves.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gloves : MonoBehaviour
+{   public GameObject hand;
+    Vector3 Offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+      
+        Offset = transform.position - hand.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = hand.transform.position+Offset;
+    }
+}
