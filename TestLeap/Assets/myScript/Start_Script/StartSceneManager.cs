7cbc9a9a03c57ac1ee344ebcae38d63d88d4f0c4
kkiyshkk0 <kkiyshkk0@gmail.com>
2020-10-02 15:43:18 +0900

    #2020-10-02 #임시 저장

---
+++ TestLeap/Assets/myScript/Start_Script/StartSceneManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartSceneManager : MonoBehaviour
+{
+    //게임 화면으로 씬 이동
+    public void Game_Start()
+    {
+        controller.score = 0;
+        controller.level = 1;
+        SceneManager.LoadScene("punch");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "hand")
+        {
+            controller.score = 0;
+            controller.level = 1;
+            SceneManager.LoadScene("punch");
+        }
+    }
+}
---
+++ TestLeap/Assets/myScript/menuButton.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuButton : MonoBehaviour
+{
+    [SerializeField] GameObject settingCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void goto_start_scene()
+    {
+
+        SceneManager.LoadScene("Start_Scene_YGC");
+    }
+    public void levelup_bt()
+    {
+        controller.score += 5;
+        controller.level++;
+    }
+    public void leveldown_bt()
+    {
+        controller.score -= 5;
+        controller.level--;
+    }
+
+    public void reStart()
+    {
+        Time.timeScale = 1f;
+        controller.heart = 30f;
+        Debug.Log("why not react...");
+        controller.score = 0;
+        controller.level = 1;
+
+        SceneManager.LoadScene("punch");
+    }
+
+    public void setting_button()
+    {
+        settingCanvas.SetActive(true);
+    }
+}
