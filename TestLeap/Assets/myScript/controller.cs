a2ad40479dfa3819a35542957c9b22df230d2f4d
kkiyshkk0 <kkiyshkk0@gmail.com>
2020-10-05 17:42:07 +0900

    #2020-10-05 #버그 해결 1차

--- TestLeap/Assets/myScript/controller.cs
+++ TestLeap/Assets/myScript/controller.cs
@@ -24,33 +24,33 @@ public class controller : MonoBehaviour
     public GameObject Langsat;
     public GameObject Slime;
     public GameObject Turtle;
-    
+
     Vector3 original_p;
     public Canvas menu;
 
-   private bool isopen = false;
-     public Canvas menu_gameover;
-     private bool isover=false;
+    private bool isopen = false;
+    public Canvas menu_gameover;
+    private bool isover = false;
 
     public float delayTime;
-    public static int mode=0; // 0/1/2 : 각각 easy/normal/hard
+    public static int mode = 0; // 0/1/2 : 각각 easy/normal/hard
     public Canvas mode_canvas;
-    
+
     // Start is called before the first frame update
 
 
     void Start()
     {
 
-       // oncam();
+        // oncam();
 
         heart = 30f;
         original_p = new Vector3(0.0f, 5.0f, 10f);
         StartCoroutine(continueing());
         delayTime = 3.0f;
-        
-                menu_gameover.enabled = false;
-                 mode_canvas.enabled=false;
+
+        menu_gameover.enabled = false;
+        mode_canvas.enabled = false;
 
     }
     void Update()
@@ -60,8 +60,8 @@ public class controller : MonoBehaviour
         {
             Time.timeScale = 0;
 
-                menu_gameover.enabled = true;
-                //Instantiate(menu);
+            menu_gameover.enabled = true;
+            //Instantiate(menu);
 
         }
         if (Input.GetKeyDown("escape"))
@@ -69,49 +69,55 @@ public class controller : MonoBehaviour
             isopen = !isopen;
             if (isopen)
             {
-                mode_canvas.enabled=true;
+                // mode_canvas.enabled = true;
                 Time.timeScale = 0.001f;
                 menu.enabled = true;
                 //Instantiate(menu);
             }
             else
-            {mode_canvas.enabled=false;
+            {
+                // mode_canvas.enabled = false;
                 menu.enabled = false;
                 Time.timeScale = 1f;
             }
         }
-        
+
 
         score_text.text = "score : " + score;   // 점수 갱신
         level_text.text = "Lv : " + level;   // 점수 갱신
     }
-    
 
-  void instantiate_Prefab(GameObject Prefab){
-            Instantiate(Prefab, new Vector3(original_p.x + Random.Range(-15f, 20f),
-                                                original_p.y + Random.Range(-5f, 5f),
-                                                original_p.z + Random.Range(-40f, 20f)),
-                                                Quaternion.identity);
-  }
+
+    void instantiate_Prefab(GameObject Prefab)
+    {
+        Instantiate(Prefab, new Vector3(original_p.x + Random.Range(-15f, 20f),
+                                            original_p.y + Random.Range(-5f, 5f),
+                                            original_p.z + Random.Range(-40f, 20f)),
+                                            Quaternion.identity);
+    }
 
     IEnumerator continueing()
     {
         while (true)
-        {float delay=delayTime/level;
+        {
+            float delay = delayTime / level;
             // 랜덤한 위치에 따라 타격 오브젝트 생성
-            if(delay<1.5f){ delay=1.5f;}
-             instantiate_Prefab(Turtle);
-             instantiate_Prefab(Kiwi);
-                       
-            if(level>4){
-            instantiate_Prefab(Chili);
-            instantiate_Prefab(Eggy);
-            delay=2.0f;
-                                  
-            } if(level>9){
+            if (delay < 1.5f) { delay = 1.5f; }
+            instantiate_Prefab(Turtle);
+            instantiate_Prefab(Kiwi);
+
+            if (level > 4)
+            {
+                instantiate_Prefab(Chili);
+                instantiate_Prefab(Eggy);
+                delay = 2.0f;
+
+            }
+            if (level > 9)
+            {
                 instantiate_Prefab(Langsat);
-                instantiate_Prefab(Slime);                
-                 delay=2.5f;                       
+                instantiate_Prefab(Slime);
+                delay = 2.5f;
             }
             yield return new WaitForSeconds(delay);
         }
--- TestLeap/Assets/myScript/menuButton.cs
+++ TestLeap/Assets/myScript/menuButton.cs
@@ -6,51 +6,63 @@ using UnityEngine.UI;
 
 public class menuButton : MonoBehaviour
 {
-        public Text mode_text;
-    
+    [SerializeField] GameObject Setting_Canvas;
+    public Text mode_text;
+
     // Start is called before the first frame update
     void Start()
     {
-          controller.mode=0;
-          mode_text.text="easy";
+        controller.mode = 0;
+        mode_text.text = "easy";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    public void goto_start_scene(){
-        
+    public void goto_start_scene()
+    {
+
         SceneManager.LoadScene("Start_Scene_YGC");
     }
-    public void levelup_bt(){
-        controller.score+=5;
+    public void levelup_bt()
+    {
+        controller.score += 5;
         controller.level++;
     }
-    public void leveldown_bt(){
-         controller.score-=5;
+    public void leveldown_bt()
+    {
+        controller.score -= 5;
         controller.level--;
     }
-    public void easy_mode(){
-        controller.mode=0;
-        mode_text.text="easy";
+    public void easy_mode()
+    {
+        controller.mode = 0;
+        mode_text.text = "easy";
     }
-      public void normal_mode(){
-        controller.mode=1;
-        mode_text.text="normal";
+    public void normal_mode()
+    {
+        controller.mode = 1;
+        mode_text.text = "normal";
     }
-      public void hard_mode(){
-        controller.mode=2;
-        mode_text.text="hard";
+    public void hard_mode()
+    {
+        controller.mode = 2;
+        mode_text.text = "hard";
     }
 
-    public  void  reStart()
-    {   Time.timeScale = 1f;
-        controller.heart=30f;
+    public void setting_button()
+    {
+        Setting_Canvas.SetActive(true);
+    }
+    public void reStart()
+    {
+        Time.timeScale = 1f;
+        controller.heart = 30f;
         Debug.Log("why not react...");
-        controller.score=0;
-        controller.level=1;
+        controller.score = 0;
+        controller.level = 1;
 
         SceneManager.LoadScene("punch");
     }
