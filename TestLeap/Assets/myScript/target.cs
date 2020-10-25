055c92c2931ff3b5374d91681b39d07d99d41e3e
kkiyshkk0 <kkiyshkk0@gmail.com>
2020-10-25 17:15:04 +0900

    #2020-10-25 #2차 완성

--- TestLeap/Assets/myScript/target.cs
+++ TestLeap/Assets/myScript/target.cs
@@ -16,25 +16,25 @@ public class target : MonoBehaviour
     float target_speed;
     Renderer renderer;
     // Start is called before the first frame update
-         private Vector3 LastPos;
+    private Vector3 LastPos;
     public Vector3 CurrentSpeed;// { get; private set; }
                                 // Start is called before the first frame update
     void Start()
     {
-    LastPos = transform.position;
-    CurrentSpeed = transform.position;
-// 속도 측정 준비
+        LastPos = transform.position;
+        CurrentSpeed = transform.position;
+        // 속도 측정 준비
 
 
-        
-    transform.rotation=Quaternion.Euler(0,180,0);
 
-    target_speed=5f*(1+((float)controller.level/20f));// 원래 10 f
+        transform.rotation = Quaternion.Euler(0, 180, 0);
 
-    renderer = gameObject.GetComponent<Renderer>();
-       // hp = (int)Random.Range(1f, 4f);
-    hp=1;
-    maxhp = hp;
+        target_speed = 5f * (1 + ((float)controller.level / 20f));// 원래 10 f
+
+        renderer = gameObject.GetComponent<Renderer>();
+        // hp = (int)Random.Range(1f, 4f);
+        hp = 1;
+        maxhp = hp;
 
         switch (hp)
         {
@@ -44,30 +44,31 @@ public class target : MonoBehaviour
             case 2:
                 renderer.material.color = Color.red;
                 break;
-  
-         
+
+
             default:
                 renderer.material.color = Color.black;
                 break;
         }
         r = GetComponent<Rigidbody>();
-        target_b = new Vector3(0.0f, 5.0f, 0.1f);
+        target_b = new Vector3(0.0f, 0.0f, 0.1f);
 
         original_p = transform.position;
     }
 
     // Update is called once per frame
     void Update()
-    {  Vector3 displacement = transform.position - LastPos;
+    {
+        Vector3 displacement = transform.position - LastPos;
         CurrentSpeed = displacement / Time.deltaTime;
-       // Debug.Log(CurrentSpeed.magnitude);
+        // Debug.Log(CurrentSpeed.magnitude);
         LastPos = transform.position;
-// 속도 측정완료 = CurrentSpeed
+        // 속도 측정완료 = CurrentSpeed
 
 
 
-      //  targettmp = GameObject.FindGameObjectWithTag("target");
-    //    target_b = targettmp.transform.position;
+        //  targettmp = GameObject.FindGameObjectWithTag("target");
+        //    target_b = targettmp.transform.position;
 
         // 타격 오브젝트 이동
         transform.position = Vector3.MoveTowards(transform.position, target_b, target_speed * Time.deltaTime);
@@ -85,29 +86,32 @@ public class target : MonoBehaviour
         // Hand 객체와 충돌 시 타격 오브젝트 파괴
         if (collision.gameObject.tag == "hand")
         {
-          
-               Debug.Log("coll speed in target script : "+CurrentSpeed.magnitude);
 
-switch(controller.mode){
-    case 1:
-        if(CurrentSpeed.magnitude>10){
-        hp -= 1;
-        Debug.Log("----------------------------------------------------10 이상 성공 normal "+CurrentSpeed.magnitude);
-        }
-    break;
-    case 2:
-        if(CurrentSpeed.magnitude>20){
-        hp -= 1;
-        Debug.Log("------------------------------------------------------------20이상 성공 hard  "+CurrentSpeed.magnitude);
-        }
-    break;
-    default :
-     Debug.Log("------------------------------------------------------------ 성공 easy  "+CurrentSpeed.magnitude);
-        hp -= 1;
-        break;
-}
+            Debug.Log("coll speed in target script : " + CurrentSpeed.magnitude);
 
-           
+            switch (controller.mode)
+            {
+                case 1:
+                    if (CurrentSpeed.magnitude > 10)
+                    {
+                        hp -= 1;
+                        Debug.Log("----------------------------------------------------10 이상 성공 normal " + CurrentSpeed.magnitude);
+                    }
+                    break;
+                case 2:
+                    if (CurrentSpeed.magnitude > 20)
+                    {
+                        hp -= 1;
+                        Debug.Log("------------------------------------------------------------20이상 성공 hard  " + CurrentSpeed.magnitude);
+                    }
+                    break;
+                default:
+                    Debug.Log("------------------------------------------------------------ 성공 easy  " + CurrentSpeed.magnitude);
+                    hp -= 1;
+                    break;
+            }
+
+
             switch (hp)
             {
                 case 1:
@@ -127,11 +131,11 @@ switch(controller.mode){
                 controller.heart += maxhp;
                 controller.score += maxhp;
 
-              
-                    controller.level = (controller.score / 5) +1;
-                    
-                
-                    GetComponent<MeshExploder>().Explode();// 깨지는 이미지 구현한거 
+
+                controller.level = (controller.score / 5) + 1;
+
+
+                GetComponent<MeshExploder>().Explode();// 깨지는 이미지 구현한거 
                 this.gameObject.SetActive(false);
                 Destroy(this.gameObject);
             }
