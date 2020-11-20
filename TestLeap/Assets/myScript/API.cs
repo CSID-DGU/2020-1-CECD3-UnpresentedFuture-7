57f16a831b525ce7bf1702ba3f47b6df94a43ac8
kkiyshkk0 <kkiyshkk0@gmail.com>
2020-11-20 12:33:14 +0900

    #2020-11-20 #1차

--- TestLeap/Assets/myScript/API.cs
+++ TestLeap/Assets/myScript/API.cs
@@ -24,24 +24,12 @@ public class API : MonoBehaviour
     // Start is called before the first frame update
     public void Start()
     {
-        // Set up the Editor before calling into the realtime database.
-        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://arboxingfb.firebaseio.com/");
-
-        // Get the root reference location of the database.
-        reference = FirebaseDatabase.DefaultInstance.RootReference;
-
-        //User temp = getUserById(4);
-        //print("NAME: " + temp.userName + "SCORE: " + temp.score + "ID: " + temp.id);
+        Reset();
     }
 
-    public DatabaseReference reference { get; set; } 
+    public DatabaseReference reference { get; set; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    void Reset()
+    public void Reset()
     {
         Debug.Log("EXcute reset!");
         // Set up the Editor before calling into the realtime database.
@@ -53,7 +41,7 @@ public class API : MonoBehaviour
     public void addNewUser(string userName, double score, double id)
     {
         print("addUser!");
-        
+
         DateTime dt2 = new DateTime();
         dt2 = DateTime.Now;
 
@@ -63,7 +51,7 @@ public class API : MonoBehaviour
         string json = JsonUtility.ToJson(user);
 
         print(reference);
-       
+
         reference.Child("Users").Child(id.ToString()).SetRawJsonValueAsync(json);
     }
 
@@ -86,7 +74,7 @@ public class API : MonoBehaviour
                     User MyUser = new User();
                     IDictionary user = (IDictionary)data.Value;
                     //Debug.Log("NAME: " + user["userName"] + "SCORE: " + user["score"] + "ID: " + user["id"]);
-                    print("NAME: " + user["userName"] + "SCORE: " + user["score"] + "ID: " + user["id"] + "DATE: " + user["dateString"]);
+                    print("NAME: " + user["userName"] + "\tSCORE: " + user["score"] + "\tID: " + user["id"] + "\tDATE: " + user["dateString"]);
 
                     string name = "" + user["userName"];
 
@@ -94,12 +82,13 @@ public class API : MonoBehaviour
                     MyUser.score = Convert.ToInt32("" + user["score"]);
                     MyUser.id = Convert.ToInt32("" + user["id"]);
                     MyUser.dateString = "" + user["dateString"];
-                    
+
                     users.Add(MyUser);
                 }
                 callback.Invoke(users.ToArray());
 
-            } else
+            }
+            else
             {
                 Debug.Log("false");
                 print("false");
@@ -115,14 +104,14 @@ public class API : MonoBehaviour
         FirebaseDatabase.DefaultInstance.GetReference("Users")
             .OrderByChild("id").EqualTo(id).GetValueAsync().ContinueWith(task =>
         {
-            
+
             if (task.IsCompleted)
             {
                 DataSnapshot snapShot = task.Result;
-   
+
                 print("the number of count is " + snapShot.ChildrenCount);
 
-                foreach(DataSnapshot data in snapShot.Children)
+                foreach (DataSnapshot data in snapShot.Children)
                 {
 
                     IDictionary user = (IDictionary)data.Value;
@@ -136,22 +125,67 @@ public class API : MonoBehaviour
                     MyUser.id = Convert.ToInt32("" + user["id"]);
                 }
 
-            } else
+            }
+            else
             {
                 Debug.Log("false");
                 print("false");
             }
-            
+
         });
 
         return MyUser;
     }
+
+    //score점수로 정렬 출력 : YGC
+    public void getUserByScore(UnityAction<User[]> callback)
+    {
+        print("these are sorted datas by score");
+
+        FirebaseDatabase.DefaultInstance.GetReference("Users")
+            .OrderByChild("scoreForSort").LimitToFirst(3).GetValueAsync().ContinueWith(task =>
+        {
+            List<User> users = new List<User>();
+            if (task.IsCompleted)
+            {
+                DataSnapshot snapShot = task.Result;
+                
+
+                print("the number of count is " + snapShot.ChildrenCount);
+
+                foreach (DataSnapshot data in snapShot.Children)
+                {
+                    User MyUser = new User();
+                    IDictionary user = (IDictionary)data.Value;
+                    //Debug.Log("NAME: " + user["userName"] + "SCORE: " + user["score"] + "ID: " + user["id"]);
+                    print("NAME: " + user["userName"] + "\tSCORE: " + user["score"] + "\tID: " + user["id"] + "\tDATE: " + user["dateString"]);
+
+                    string name = "" + user["userName"];
+
+                    MyUser.userName = "" + user["userName"];
+                    MyUser.score = Convert.ToInt32("" + user["score"]);
+                    MyUser.id = Convert.ToInt32("" + user["id"]);
+                    MyUser.dateString = "" + user["dateString"];
+
+                    users.Add(MyUser);
+                }
+                callback.Invoke(users.ToArray());
+            }
+            else
+            {
+                Debug.Log("false");
+                print("false");
+            }
+        });
+    }
 }
+
 [Serializable]
 public class User
 {
     public string userName;
     public double score;
+    public double scoreForSort;
     public double id;
     public string dateString;
 
@@ -159,6 +193,7 @@ public class User
     {
         this.userName = "NoName";
         this.score = 0;
+        this.scoreForSort = 0;
         this.id = 0;
         this.dateString = "2020-11-16 12:00:00";
     }
@@ -167,6 +202,7 @@ public class User
     {
         this.userName = userName;
         this.score = score;
+        this.scoreForSort = 0 - score;
         this.id = id;
         this.dateString = dateString;
     }
--- TestLeap/Assets/myScript/Mocker.cs
+++ TestLeap/Assets/myScript/Mocker.cs
@@ -6,49 +6,92 @@ using UnityEngine.UI;
 public class Mocker : MonoBehaviour
 {
     [SerializeField] private bool fire;
+    [SerializeField] private bool fire2;
+    [SerializeField] private bool reset;
+    [SerializeField] private bool dataAdd;
 
     [SerializeField] API data;
-    [SerializeField] UnityEngine.Events.UnityEvent Todo, BeforeLoad, AfterLoad;
 
-    public Text username1;
-    public Text username2;
-    public Text username3;
-    public Text userScore1;
-    public Text userScore2;
-    public Text userScore3;
+    private User[] users;
+    [SerializeField] Text[] userNick;
+    [SerializeField] Text[] userScore;
 
+    void Start()
+    {
+        users = new User[3];
+        for (int i = 0; i < users.Length; i++)
+        {
+            users[i] = new User();
+        }
+
+        getUserDatabyScore();
+    }
+
+    void Update()
+    {
+        printRanking(users);
+    }
 
     void OnValidate()
     {
         if (fire)
         {
-            Debug.Log("데이터베이스 호출");
-            fire = false;
+            getUserData();
+        }
+        else if (fire2)
+        {
+            getUserDatabyScore();
+        }
+        else if (reset)
+        {
+            Debug.Log("데이터베이스 리셋!!!");
+            reset = false;
 
-            // maruta.addNewUser("제임스", 54, 1);
-            User[] users;
-            data.getUser((User[] users2) =>
+            data.Reset();
+        }
+        else if (dataAdd)
+        {
+            Debug.Log("데이터 추가");
+            dataAdd = false;
+
+            data.addNewUser("이현재", 10, 1);
+            data.addNewUser("리윤우", 320, 2);
+            data.addNewUser("윤기철", 210, 3);
+            data.addNewUser("륜승규", 40, 4);
+        }
+    }
+
+    private void getUserData()
+    {
+        Debug.Log("데이터베이스 호출");
+        fire = false;
+
+        data.getUser((User[] users2) =>
+        {
+            users = users2;
+        });
+    }
+
+    private void getUserDatabyScore()
+    {
+        Debug.Log("데이터베이스 호출 _ 점수 정렬");
+        fire2 = false;
+
+        data.getUserByScore((User[] users2) =>
+        {
+            for (int i = 0; i < users.Length; i++)
             {
                 users = users2;
-                print(users[0].userName);
-                print(users[1].userName);
-                print(users[2].userName);
+            }
+        });
+    }
 
-                AfterLoad.Invoke();
-
-            });
-            BeforeLoad.Invoke();
-
-            //print(maruta.getUserById(4).userName);
-            Todo.Invoke();
-            //print( maruta.getUserById(4).userName);
-
-            // username1.text = users[0].userName;
-            // username2.text = users[1].userName;
-            // username3.text = users[2].userName;
-            // userScore1.text = users[0].score.ToString();
-            // userScore2.text = users[1].score.ToString();
-            // userScore3.text = users[2].score.ToString();
+    public void printRanking(User[] users)
+    {
+        for (int i = 0; i < userNick.Length; i++)
+        {
+            userNick[i].text = users[i].userName;
+            userScore[i].text = users[i].score.ToString();
         }
     }
 }
